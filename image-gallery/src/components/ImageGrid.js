8cc5002de06cf2c15fe4e1d8b008020282a2e29a
Supuni22 <supunimanamperi@gmail.com>
2020-11-21 14:09:47 +0530

    Styles changed

--- image-gallery/src/components/ImageGrid.js
+++ image-gallery/src/components/ImageGrid.js
@@ -7,7 +7,7 @@ const ImageGrid = () => {
     console.log(docs)
 
     return (
-        <div className='image-grid'>
+        <div className='img-grid'>
            {docs && docs.map(doc => (
                <div className='img-wrap' key={doc.id}>
                    <img src={doc.url} alt='uploaded pic'/>
