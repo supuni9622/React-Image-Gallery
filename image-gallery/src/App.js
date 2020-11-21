3ea50681a8d5cf3b398a121f785bcfd630317fd3
Supuni22 <supunimanamperi@gmail.com>
2020-11-21 11:16:40 +0530

    Created upload form layout

--- image-gallery/src/App.js
+++ image-gallery/src/App.js
@@ -1,9 +1,11 @@
 import Title from './components/Title'
+import UploadForm from './components/UploadForm'
 
 const App = () => {
   return (
     <div className="App">
       <Title/>
+      <UploadForm/>
     </div>
   );
 }
---
+++ image-gallery/src/components/UploadForm.js
@@ -0,0 +1,49 @@
+import React, {useState,useCallback, useMemo} from 'react'
+
+const UploadForm = () => {
+
+    const [file, setFile] = useState(null)
+    const [error, setError] =useState('')
+
+    const types = useMemo(() => ['image/jpeg', 'image/png'], [])
+
+    const onChangeHandle = useCallback((e) => {
+        
+        const selected = e.target.files[0]
+        console.log(selected)
+
+        // Check if one file uploaded and that file is type of png or jpeg
+        if(selected && types.includes(selected.type)){
+           setFile(selected)
+           setError('')
+        }else {
+            setFile(null)
+            setError("Please select an image file (png or jpeg")
+        }
+
+    },[setFile,types])
+
+    return (
+        <div>
+            <form>
+                <input type='file' onChange={onChangeHandle}/>
+            </form>
+            <div className='output'>
+                {error && 
+                    <div className='error'>
+                        {error}
+                    </div>
+                }
+                {file && 
+                    <div className=''>
+                        {file.name}
+                    </div>
+                }
+            </div>
+        </div>
+    )
+}
+
+
+export default UploadForm
+
